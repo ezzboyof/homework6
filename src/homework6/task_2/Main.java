5eb4ce239a6ea8fb9c41143faea2297d88ac372c
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-27 14:56:18 +0300

    Update Main.java

--- src/homework6/task_2/Main.java
+++ src/homework6/task_2/Main.java
@@ -2,7 +2,7 @@ package homework6.task_2;
 
 public class Main {
     public static void main(String[] args) {
-
+    // пустая строка не нужна
         Car car = new Car();
         car.ride();
         car.getTransmission().changeGearUp();
